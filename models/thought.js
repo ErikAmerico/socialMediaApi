4bc2a7f19fffbcfc7297c48d4e5c0289524a5bf5
Erik Olson <erikamerico1991@gmail.com>
2023-08-09 11:12:34 -0400

    added models

---
+++ models/thought.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const ReactionSchema = require('./reaction');
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => new Date(createdAtVal).toLocaleString()
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [ReactionSchema]
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
